1bacb056530801f8ec7197cdcea76ab6394d7f2d
Derek Chu <derekchu53789@gmail.com>
2019-12-08 14:17:45 -0800

    Added Authentication, Fixed Dependencies
    Added Authentication according to this Medium article: https://medium.com/react-native-training/react-native-firebase-authentication-7652e1d2c8a2. Fixed dependencies and removed reliance on npm.

---
+++ app/Pages/Main.js
@@ -0,0 +1,31 @@
+// Main.js
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import auth from '@react-native-firebase/auth';
+
+export default class Main extends React.Component {
+  state = { currentUser: null }
+
+  componentDidMount() {
+    const { currentUser } = auth()
+    this.setState({ currentUser })
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
