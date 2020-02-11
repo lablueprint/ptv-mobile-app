0df8488712c4a1daaec49644d0ee5cba93227437
Ryan Tran <ryanmtran922@gmail.com>
2020-02-11 01:00:58 -0800

    Added Interface for Forum Navigation

--- src/screens/Forum/index.js
+++ src/screens/Forum/index.js
@@ -1,5 +1,8 @@
 import ForumHomeScreen from './ForumHomeScreen';
 import ForumCategoriesScreen from './ForumCategoriesScreen';
 import ForumPostScreen from './ForumPostScreen';
+import CreateForumPostScreen from './CreateForumPostScreen';
 
-export { ForumHomeScreen, ForumCategoriesScreen, ForumPostScreen };
+export {
+  ForumHomeScreen, ForumCategoriesScreen, ForumPostScreen, CreateForumPostScreen,
+};
