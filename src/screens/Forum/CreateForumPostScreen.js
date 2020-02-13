1b35c44400fe2b04fab79f73eb28cde14aa8e36b
Ryan Tran <ryanmtran922@gmail.com>
2020-02-12 20:26:28 -0800

    Added Backend that sends message info to firebase, functionality that clears data when button is pressed, and errors when invalid message titles or boies are inputted

--- src/screens/Forum/CreateForumPostScreen.js
+++ src/screens/Forum/CreateForumPostScreen.js
@@ -2,11 +2,16 @@ import React from 'react';
 import {
   View, TextInput, Text, Button,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 
 const INITIAL_STATE = {
   title: '',
   body: '',
+  errorMessageTitle: null,
+  errorMessageBody: null,
+  userID: '',
 };
 
 export default class CreateForumPostScreen extends React.Component {
@@ -16,17 +21,50 @@ export default class CreateForumPostScreen extends React.Component {
     this.handleOnClick = this.handleOnClick.bind(this);
   }
 
-  handleOnClick() {
-    const { title, body } = this.state;
-    console.log('title');
-    console.log(title);
-    console.log('body');
-    console.log(body);
+  componentDidMount() {
+    this.unsubscribe = auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ userID: user.uid });
+      }
+    });
   }
+
+
+  handleOnClick() {
+    const {
+      title, body, userID,
+    } = this.state;
+    if (title.length === 0) {
+      this.setState({ errorMessageTitle: 'Please Enter Title Before Submitting' });
+    } else {
+      this.setState({ errorMessageTitle: null });
+    }
+    if (body.length === 0) {
+      this.setState({ errorMessageBody: 'Please Enter Text Before Submitting' });
+    } else {
+      this.setState({ errorMessageBody: null });
+    }
+    if (title.length > 0 && body.length > 0) {
+      const ref = firestore().collection('forum_posts');
+      ref.add({
+        UserID: userID,
+        Title: title,
+        Body: body,
+        Author: '',
+        createdAt: firestore.Timestamp.now(),
+      })
+        .catch((error) => {
+          console.error('Error adding document: ', error);
+        });
+      this.setState({ title: '' });
+      this.setState({ body: '' });
+    }
+  }
+
 
   render() {
     const {
-      title, body,
+      title, body, errorMessageTitle, errorMessageBody,
     } = this.state;
     return (
       <View>
@@ -36,12 +74,24 @@ export default class CreateForumPostScreen extends React.Component {
           placeholder="Enter Title"
           value={title}
         />
+        {errorMessageTitle
+          && (
+          <Text style={{ color: 'red' }}>
+            {errorMessageTitle}
+          </Text>
+          )}
         <Text>Body</Text>
         <TextInput
           onChangeText={(text) => this.setState({ body: text })}
-          placeholder="Enter Body of Message"
+          placeholder="Enter Forum Post"
           value={body}
         />
+        {errorMessageBody
+          && (
+          <Text style={{ color: 'red' }}>
+            {errorMessageBody}
+          </Text>
+          )}
         <Button
           title="Submit"
           onPress={this.handleOnClick}
