c3f06d0d354239b2b4a3524aff8d94b1bc996b63
Derek Chu <derekchu53789@gmail.com>
2020-02-23 16:54:27 -0800

    Added password reset
    Got rid of Edit Profile button on HomeScreen

--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,11 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import SignInScreen from '../screens/SignIn';
 import SignUpScreen from '../screens/SignUp';
+import ForgotPasswordScreen from '../screens/ForgotPassword';
 
 const AuthNavigation = createStackNavigator(
   {
     SignInScreen,
     SignUpScreen,
+    ForgotPasswordScreen,
   },
   {
     initialRouteName: 'SignInScreen',
