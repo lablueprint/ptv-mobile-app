bea31db0d69807e543ad1d57eb14ec0ddc7f676b
bianca villamor <biancavillamor99@biancas-mbp-2.home>
2020-02-25 21:08:18 -0800

    initial backend work for forum categories

--- src/screens/Forum/ForumCategoriesScreen.js
+++ src/screens/Forum/ForumCategoriesScreen.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { ScrollView, Text } from "react-native";
+import { blue100 } from "react-native-paper/lib/typescript/src/styles/colors";
+
+const styles = StyleSheet.create({
+  categories: {
+    backgroundColor: blue100
+  }
+});
 
 export default function ForumCategoriesScreen() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [forumCategories, setCategoryList] = useState([]);
+
+  firestore()
+    .collection("forum_categories")
+    .get()
+    .then(snapshot => {
+      const forum_categories = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      setCategoryList(
+        forum_categories
+          .sort((a, b) => {
+            const aMillis = a.time.toMillis();
+            const bMillis = b.time.toMillis();
+            if (aMillis > bMillis) {
+              return -1;
+            }
+            if (aMillis < bMillis) {
+              return 1;
+            }
+            return 0;
+          })
+          .map(forum_categories => {
+            return (
+              <TouchableOpacity style={styles.categories}>
+                {forum_categories.forum_categories}
+              </TouchableOpacity>
+              // to do: create component for forum categories
+            );
+          })
+      );
+    })
+    .catch(error => {
+      setErrorMessage(error.message);
+    });
+
   return (
-    <View>
-      <Text>Not implemented</Text>
-    </View>
+    <ScrollView>
+      {errorMessage && <Text>{errorMessage}</Text>}
+      {forumCategories}
+    </ScrollView>
   );
 }
