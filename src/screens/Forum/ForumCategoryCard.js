ac575d74aced962f17f69a635cb5de2a2b3e98c5
bianca villamor <biancavillamor99@biancas-mbp-2.home>
2020-03-03 20:36:59 -0800

    cleaned up initial forum category card component and forum category screen

---
+++ src/screens/Forum/ForumCategoryCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Title } from 'react-native-paper';
+import {
+  View, TouchableOpacity, StyleSheet,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    padding: 30,
+    backgroundColor: '#ffffff',
+    borderRadius: 15,
+  },
+  text: {
+    alignSelf: 'center',
+  },
+});
+
+export default function ForumCategoryCard({
+  children,
+}) {
+  return (
+    <TouchableOpacity>
+      <View style={styles.container}>
+        <Title style={styles.text}>
+          {children}
+        </Title>
+      </View>
+    </TouchableOpacity>
+  );
+}
+ForumCategoryCard.propTypes = {
+  children: PropTypes.string.isRequired,
+};
