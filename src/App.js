ca88beaa81613e6bba3a4996cb723cb5a773ab60
Bill Liu <bill.liu.school@gmail.com>
2020-03-10 19:37:43 -0700

    Fixed merge conflict with PaperProviderin App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme, configureFonts } from 'react-native-paper';
 import AppContainer from './navigation';
+
+const fontConfig = {
+  default: {
+    regular: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'sans-serif-medium',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+};
+
+const theme = {
+  ...DefaultTheme,
+  fonts: configureFonts(fontConfig),
+};
 
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <AppContainer />
     </PaperProvider>
   );
