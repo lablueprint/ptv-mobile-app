359f20ad6bcc609ef467b065a407db6f64aaf47f
Bill Liu <bill.liu.school@gmail.com>
2020-04-02 21:52:50 -0400

    Basic implementation of wikihow-style steps

--- src/screens/Home/ResourcesItemScreen.js
+++ src/screens/Home/ResourcesItemScreen.js
@@ -5,7 +5,7 @@ import {
 import PropTypes from 'prop-types';
 import firestore from '@react-native-firebase/firestore';
 import {
-  Title, Subheading, Text, Paragraph,
+  Title, Subheading, Text, Paragraph, List,
 } from 'react-native-paper';
 import { theme } from '../../style';
 
@@ -26,6 +26,17 @@ const resourcesStyles = StyleSheet.create({
   },
 });
 
+function ResourceStep({
+  stepNumber, title,
+}) {
+  return (
+    <List.Item
+      title={title}
+      icon={`numeric-${stepNumber}-circle`}
+    />
+  );
+}
+
 export default function ResourcesItemScreen(props) {
   const { navigation } = props;
   const resourceID = navigation.getParam('resourceID');
@@ -34,6 +45,7 @@ export default function ResourcesItemScreen(props) {
   const [description, setDescription] = useState(null);
   const [type, setType] = useState(null);
   const [body, setBody] = useState(null);
+  const [steps, setSteps] = useState(null);
 
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -48,16 +60,30 @@ export default function ResourcesItemScreen(props) {
       setTitle(resourceData.title);
       setDescription(resourceData.description);
       setType(resourceData.type);
-      switch (type) {
-        case 'STEPS':
-          // TODO
-          break;
-        case 'FREEFORM':
-          setBody(resourceData.body);
-          break;
-        default:
-          // TODO
-          break;
+      if (type === 'STEPS') {
+        const stepsList = await firestore().collection('resource_items').doc(resourceID).collection('steps')
+          .get();
+        const stepsData = stepsList.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+        const sortedStepsData = stepsData.sort((a, b) => {
+          if (a.stepNumber > b.stepNumber) {
+            return 1;
+          }
+          if (a.stepNumber < b.stepNumber) {
+            return -1;
+          }
+          return 0;
+        });
+
+        setSteps(sortedStepsData.map((step) => ({
+          ...(<ResourceStep
+            stepNumber={step.stepNumber}
+            title={step.title}
+          />),
+          key: step.id,
+        })));
+      } else if (type === 'FREEFORM') {
+        setBody(resourceData.body);
       }
 
       setLoading(false);
@@ -81,7 +107,13 @@ export default function ResourcesItemScreen(props) {
           <Paragraph>{description}</Paragraph>
           <Text>{'\n'}</Text>
           <Subheading>Body/Steps</Subheading>
-          <Paragraph>{body}</Paragraph>
+          {steps != null
+            ? (
+              <List.Section>
+                {steps}
+              </List.Section>
+            )
+            : <Paragraph>{body}</Paragraph>}
         </View>
       </ScrollView>
     </View>
@@ -94,4 +126,9 @@ ResourcesItemScreen.propTypes = {
     navigate: PropTypes.func.isRequired,
     push: PropTypes.func.isRequired,
   }).isRequired,
+};
+
+ResourceStep.propTypes = {
+  stepNumber: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
 };
