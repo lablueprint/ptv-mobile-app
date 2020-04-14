be003b2fea4b68c983e84d23cd8c76d0f7ab5297
hwang635 <hejwa9@yahoo.com>
2020-04-13 18:42:59 -0700

    Place categories query in useEffect hook

--- src/screens/Forum/ForumCategoriesScreen.js
+++ src/screens/Forum/ForumCategoriesScreen.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ScrollView, Text, View, StyleSheet,
 } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import PropTypes from 'prop-types';
 import ForumCategoryCard from './ForumCategoryCard';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -13,35 +14,36 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default function ForumCategoriesScreen({ navigation }) {
   const [errorMessage, setErrorMessage] = useState(null);
   const [forumCategories, setForumCategories] = useState([]);
 
-  firestore()
-    .collection('forum_categories')
-    .orderBy('title')
-    .get()
-    .then((snapshot) => {
-      const forumCategoriesData = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setForumCategories(
-        forumCategoriesData
-          .map((forum) => {
-            const navigateToSubcategory = () => navigation.navigate('ForumSubcategoryPosts', { categoryID: forum.id });
-            return (
-              <ForumCategoryCard key={forum.id} navigate={navigateToSubcategory}>
-                {forum.title}
-              </ForumCategoryCard>
-            );
-          }),
-      );
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-    });
+  useEffect(() => { /* Put firestore query inside useEffect hook to prevent infinite loop */
+    firestore()
+      .collection('forum_categories')
+      .orderBy('title')
+      .get()
+      .then((snapshot) => {
+        const forumCategoriesData = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setForumCategories(
+          forumCategoriesData
+            .map((forum) => {
+              const navigateToSubcategory = () => navigation.navigate('ForumSubcategoryPosts', { categoryID: forum.id });
+              return (
+                <ForumCategoryCard key={forum.id} navigate={navigateToSubcategory}>
+                  {forum.title}
+                </ForumCategoryCard>
+              );
+            }),
+        );
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  });
 
   return (
     <View style={styles.container}>
