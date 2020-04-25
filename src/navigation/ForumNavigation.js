359f818d6dfc829dd6f3e655986d4d9d15d37ef1
hwang635 <hejwa9@yahoo.com>
2020-04-24 21:13:25 -0700

    Added fonts, colours, formatting to forum stack navig

--- src/navigation/ForumNavigation.js
+++ src/navigation/ForumNavigation.js
@@ -4,14 +4,25 @@ import {
   ForumHomeScreen, ForumCategoriesScreen, ForumPostScreen, ForumSubcategoryPostsScreen,
   CreateForumPostScreen,
 } from '../screens/Forum';
+import { theme } from '../style';
 
 const ForumHomeTopTabNavigation = createMaterialTopTabNavigator(
   {
     ForumHome: {
       screen: ForumHomeScreen,
+      navigationOptions: () => ({
+        title: 'All Posts',
+      }),
     },
     ForumCategories: {
       screen: ForumCategoriesScreen,
+      navigationOptions: () => ({
+        title: 'Categories',
+      }),
+    },
+  },
+  {
+    defaultNavigationOptions: {
     },
   },
 );
@@ -20,15 +31,33 @@ const ForumNavigation = createStackNavigator(
   {
     ForumHome: {
       screen: ForumHomeTopTabNavigation,
+      navigationOptions: () => ({ title: 'Home' }),
     },
     ForumPost: {
       screen: ForumPostScreen,
+      navigationOptions: () => ({ title: 'Post' }),
     },
     ForumSubcategoryPosts: {
       screen: ForumSubcategoryPostsScreen,
+      navigationOptions: () => ({ title: 'Category' }),
     },
     CreateForumPost: {
       screen: CreateForumPostScreen,
+      navigationOptions: () => ({ title: 'New Post' }),
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#3190D0',
+      },
+      headerTintColor: '#FFFFFF',
+      headerTitleAlign: 'center',
+      headerTitleStyle: {
+        fontFamily: theme.fonts.regular.fontFamily,
+        fontWeight: '600',
+        fontSize: 20,
+      },
     },
   },
   {
