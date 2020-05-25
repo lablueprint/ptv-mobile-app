1e7214609952131c51143fdd5383da5ca0c29411
Vincent Cheung <vfhcheung@gmail.com>
2020-05-24 22:01:17 -0700

    On app start, wait for the current user to be loaded before rendering anything. This lets us use `const { currentUser } = auth()` if we need to get the current user in our components (no more `onAuthStateChanged()`!) See HomeScreen.js for an example of how to use this.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
 import AppContainer from './navigation';
 import { theme } from './style';
 
-
 export default function App() {
+  const [authLoading, setAuthLoading] = useState(true);
+
+  useEffect(() => auth().onAuthStateChanged(() => {
+    setAuthLoading(false);
+  }), []);
+
   return (
     <PaperProvider theme={theme}>
-      <AppContainer />
+      {!authLoading && <AppContainer />}
     </PaperProvider>
   );
 }
